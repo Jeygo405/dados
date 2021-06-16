acd38998913c9ec4d4bc8b9849465196092ecb3f
jeygo405 <num.jey98@gmail.com>
2021-06-16 14:10:00 -0500

    se agrego numero de caras

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import random
 print("cuantos dados desea lanzar?")
 dados=int(input("inserte numero de dados: "))
+print("cuantos caras tiene el dado")
+caras=int(input("inserte numero de dcaras: "))
 x=0
 for x in range(0,dados):
     x=x+1
-    numero= random.randint(1,6)
+    numero= random.randint(1,caras)
     print(numero)
